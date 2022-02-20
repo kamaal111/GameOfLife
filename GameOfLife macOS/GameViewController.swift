fe16809dd87a974d720cbc9d82819c4ffdada48d
kamaal111 <kamaal.f1@gmail.com>
2022-02-20 20:25:21 +0100

    Starting from scratch with sprite kit template

---
+++ GameOfLife macOS/GameViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GameViewController.swift
+//  GameOfLife macOS
+//
+//  Created by Kamaal M Farah on 20/02/2022.
+//
+
+import Cocoa
+import SpriteKit
+import GameplayKit
+
+class GameViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let scene = GameScene.newGameScene()
+        
+        // Present the scene
+        let skView = self.view as! SKView
+        skView.presentScene(scene)
+        
+        skView.ignoresSiblingOrder = true
+        
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+    }
+
+}
+
